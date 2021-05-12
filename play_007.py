b367ff6bc17e9705975066b85618b5d78c5f8492
“Thor” <hmkthor@gmail.com>
2021-05-12 23:10:32 +0900

    One COMMIT a day

--- play_007.py
+++ play_007.py
@@ -1,1 +1,11 @@
-#another a
+#https://edabit.com/challenge/Zerwo2AENbvRZTe83
+
+
+#Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+
+def next_edge(first_edge, second_edge):
+    return first_edge + second_edge -1
+
+
+print(next_edge(10,18))
